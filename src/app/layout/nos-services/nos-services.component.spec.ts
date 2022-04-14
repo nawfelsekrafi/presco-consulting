8da2e77b0ef78f603618bef7220764f04490b35e
Nawfel Sekrafi <48069841+nawfelsekrafi@users.noreply.github.com>
2022-04-14 21:55:25 +0100

    add nos service component

---
+++ src/app/layout/nos-services/nos-services.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NosServiceComponent } from './nos-service.component';
+
+describe('NosServiceComponent', () => {
+  let component: NosServiceComponent;
+  let fixture: ComponentFixture<NosServiceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NosServiceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NosServiceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
