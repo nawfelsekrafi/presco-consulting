3563eacd483bdca4bafde77c9a5a4c0836a1b56f
nawfelsekrafi <sekrafinawfel@gmail.com>
2022-07-26 11:12:09 +0100

    formation page finished by 100%

--- src/app/shared/ui/subscription-component/subscription.component.ts
+++ src/app/shared/ui/subscription-component/subscription.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscriptionComponent implements OnInit {
 
-  page: string = '';
+  page: string = 'inter';
   constructor() { }
 
   ngOnInit(): void {
